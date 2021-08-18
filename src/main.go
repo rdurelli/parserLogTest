b6b2f801e2bfe2677f8741fac8a93f580ee7a7aa
rafadurelli <rafa.durelli@gmail.com>
2021-08-18 17:00:56 -0300

    remove all comment in the main.go

--- src/main.go
+++ src/main.go
@@ -6,33 +6,6 @@ import (
 	"go.uber.org/fx"
 )
 
-//var (
-//	db     *sql.DB
-//	env    lib.Env
-//	logger lib.Logger
-//)
-//
-//func init() {
-//	env = lib.NewEnv()
-//	DB, err := sql.Open("mysql", env.DBUsername+":"+env.DBPassword+"@tcp("+env.DBHost+":"+env.DBPort+")/"+env.DBName+"?parseTime=true")
-//	if err != nil {
-//		log.Fatal("Something went wrong trying to connect to the database ", err)
-//	}
-//	err = DB.Ping()
-//	if err != nil {
-//		log.Fatal("Something went wrong trying to ping to the database ")
-//	}
-//	db = DB
-//	logger = lib.NewLogger(env)
-//}
-
 func main() {
-	//s := time.Now()
-	//logs := make([]models.Log, 0)
-	//readerLog := utils.NewReaderLog(env.LogPath, &logs, db, logger)
-	//readerLog.Execute()
-	////fmt.Println(logs)
-	//fmt.Println(len(logs))
-	//fmt.Println("\nTime taken - ", time.Since(s))
 	fx.New(bootstrap.Module).Run()
 }
